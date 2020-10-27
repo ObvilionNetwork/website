618e5f3e0e2ebb8a0daa4c54a8e08bc132ee6ea7
Fatonndev <56699208+Fatonndev@users.noreply.github.com>
2020-10-27 17:56:52 +0300

    Revert "Update Home.js"
    This reverts commit 16790d34f73e28d45f6fe6d135bb96af77e0367d.

--- src/pages/Public/Home.js
+++ src/pages/Public/Home.js
@@ -1,60 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Row, Col, Carousel} from 'react-bootstrap';
 
-import "../../../assets/scss/style.scss"
-import "./app.scss"
+import Aux from "../../App/components/_Aux";
+import Navbar from "../../App/layout/Public/Navbar";
+import HomePanel from "../../App/layout/Public/HomePanel";
+import "../../assets/scss/style.scss"
 
-
-
-class Navbar extends React.Component {
-    render() {
-        return(
-            <ul className="NavPanel">
-                <li className="main">
-                    <a href="/" className="b-brand">
-                        
-                    <div><span className="b-title">Obvilion Network</span></div>
-
-                    </a>
-                </li>
-
-                <li style={{marginLeft: '4%'}} className="left">
-                    <a href="/">
-                        <p className="text">Профиль</p>
-                        <div className="arrow-2">
-                            <div className="arrow-2-top"/>
-                            <div className="arrow-2-bottom"/>
-                        </div>
-                    </a>
-                </li>
-
-                <li className="left">
-                    <a href="/">
-                        <p className="text">Магазин</p>
-                    </a>
-                </li>
-
-                <li className="left">
-                    <a href="/">
-                        <p className="text">Сервера</p>
-                    </a>
-                </li>
-
-                <li className="left">
-                    <a href="/">
-                        <p className="text">Форум</p>
-                    </a>
-                </li>
-
-                <li className="left">
-                    <a href="/">
-                        <p className="text">Помощь</p>
-                    </a>
-                </li>
-
-                <li className="right"><a href="/admin" className="feather icon-settings"/></li>
-            </ul>
-        )
-    }
+class Home extends Component{
+        
+render() {
+   return ( 
+        
+        <Aux>
+             <Navbar />
+             <HomePanel />
+        </Aux>
+        
+    )
 }
 
-export default (Navbar);
+}
+
+
+export default Home;
