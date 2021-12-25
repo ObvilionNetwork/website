3cba26ec49493def31d5bb0e72b70edecb45205c
KeviTV <zxcvbnm20042005@gmail.com>
2021-12-25 11:26:32 +0300

    feat: rules page

---
+++ src/App/layout/Public/RulesСomp.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import "./RulesComp.scss"
+
+import { rules, explanations } from "../../../pages/Public/rules.json"
+
+class RulesComp extends React.Component {
+   render() {
+      return(
+         <div className='parent-block'>
+            {
+               rules.map(doc => {
+                  return <div className="asd">
+                     <div className='title-rcs'>
+                        { doc.title }
+                     </div>
+
+                     {
+                        doc.content.map(dc => {
+                           return <div className='rule'>
+                              <r>
+                                 <r className='title-rcd'>
+                                    { dc.title }
+                                 </r>
+                                 <r className='title-rcf'>
+                                    { dc.text }
+                                 </r>
+                                 <div style={{ marginTop: dc.content !== undefined ? '0.2vw' : '0vw' }}>
+                                    {
+                                       dc.content ? dc.content.map(dv => {
+                                          return <div style={{ marginLeft: '0.9vw', marginBottom: '0.18vw'}}>
+                                             <r>
+                                                <r className='title-rcd'>
+                                                   { dv.title }
+                                                </r>
+                                                <r className='title-rcf'>
+                                                   { dv.text }
+                                                </r>
+                                             </r>
+                                          </div>
+                                       }) : null
+                                    }
+                                 </div>
+                              </r>
+                           </div>
+                        })
+                     }
+                  </div>
+               })
+            }
+         </div>
+      )
+   }
+}
+
+export default (RulesComp);
---
+++ src/pages/Public/Rules.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import Aux from "../../App/components/_Aux";
+import Navbar from "../../App/layout/Public/Navbar";
+
+import "../../assets/scss/style.scss"
+import './Cabinet.scss'
+import Contacts from "../../App/layout/Public/Contacts";
+import News from "../../App/layout/Public/News";
+import RulesComp from "../../App/layout/Public/RulesСomp";
+
+class Rules extends Component {
+   render() {
+
+      return (
+         <Aux>
+            <div className="first-bg">
+               <img src={require('../../assets/images/bg.jpg')} />
+            </div>
+
+            <div className="first-bg bgg" style={{ paddingTop: '45vw' }}>
+               <img src={require('../../assets/images/bg1.jpg')} />
+            </div>
+
+            <div className="first-bg bgg" style={{ paddingTop: '80vw' }}>
+               <img src={require('../../assets/images/bg2.jpg')} />
+            </div>
+
+            <div className="first-bg bgg" style={{ paddingTop: '100vw' }}>
+               <img src={require('../../assets/images/bg3.jpg')} />
+            </div>
+
+            <div className="first-bg" style={{ paddingTop: '136vw' }}>
+               <img src={require('../../assets/images/bg5.png')} />
+            </div>
+
+            <div className="first" style={{ height: '122.1vw' }}>
+               <Navbar />
+               <News />
+
+               <div className="title">
+                  Правила проекта Obvilion.ru
+               </div>
+
+               <div style={{ marginTop: '1vw', paddingLeft: '7vw', paddingRight: '7vw' }}>
+                  <RulesComp />
+               </div>
+            </div>
+
+            <div className="first-bg end">
+               <div style={{ marginTop: '13vw' }}>
+                  <Contacts />
+               </div>
+            </div>
+         </Aux>
+      )
+   }
+}
+
+
+export default Rules;
