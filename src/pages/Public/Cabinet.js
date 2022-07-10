52b7b24aceec6d9b53ce16009131031efda97859
KeviTV <zxcvbnm20042005@gmail.com>
2022-07-10 22:27:02 +0300

    feat: do not show cabinet if not logged in

--- src/pages/Public/Cabinet.js
+++ src/pages/Public/Cabinet.js
@@ -4,6 +4,7 @@ import Aux from "../../App/components/_Aux";
 import Navbar from "../../App/layout/Public/Navbar";
 
 import "../../assets/scss/style.scss"
+import './Oops.scss'
 import './Cabinet.scss'
 import Contacts from "../../App/layout/Public/Contacts";
 import Skin from "../../App/layout/Public/Skin";
@@ -13,6 +14,36 @@ import LCButtons from "../../App/layout/Public/LCButtons";
 
 class Cabinet extends Component {
     render() {
+        if (!window.localStorage.getItem('user')) {
+           return (
+              <Aux>
+                 <div className="first">
+                    <div className="first-bg">
+                       <img src={require('../../assets/images/bg.jpg')} />
+                    </div>
+
+                    <Navbar />
+
+                    <div className="title2" style={{ marginTop: '4vw', fontSize: '4.9vw', lineHeight: '5.2vw' }}>
+                       Авторизируйтесь, <br/> прежде чем просматривать эту страницу
+                    </div>
+
+                    <div className="title2 title3">
+                       Вы можете вернуться <br/>
+                       на главную страницу или проверить <br/>
+                       правильность своего запроса.
+                    </div>
+                 </div>
+
+                 <div className="first-bg endBg">
+                    <img src={require('../../assets/images/bg5.png')} />
+                 </div>
+                 <div className="end">
+                    <Contacts />
+                 </div>
+              </Aux>
+           )
+        }
 
         return (
             <Aux>
@@ -29,6 +60,7 @@ class Cabinet extends Component {
 
                     <Skin />
                     <Info />
+
                     <LCButtons />
                 </div>
 
